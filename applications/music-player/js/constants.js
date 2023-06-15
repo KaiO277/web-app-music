a57891fa47e44931f5bfd213065349334ec067d7
KaiO277 <nghiatran1527@gmail.com>
2023-06-15 16:54:49 +0700

    KaiO

---
+++ applications/music-player/js/constants.js
@@ -0,0 +1,9 @@
+// AIzaSyB2zc1JlpjY4hgTg7I6J9w2GrAbcMpUKHg
+const API_KEY = "AIzaSyB2zc1JlpjY4hgTg7I6J9w2GrAbcMpUKHg";
+
+const NOT_FOUND_SONG = "The song is not found";
+
+const VIDEO_YOUTUBE_ID = "video_youtube_id";
+const SELECTED_VIDEO_ID = "id-selected";
+
+const NOT_FOUND = "not found";
